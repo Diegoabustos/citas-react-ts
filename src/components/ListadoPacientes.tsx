d07c3b829d473bc85bb4625a17a5a4b8e34470de
Diego <diegoabustos24@gmail.com>
2022-05-10 18:54:41 -0500

    iterating over the array

--- src/components/ListadoPacientes.tsx
+++ src/components/ListadoPacientes.tsx
@@ -1,22 +1,25 @@
-import Paciente from './Paciente'
+import { Paciente } from "../types";
+import PacienteCita from "./Paciente";
 
-const ListadoPacientes = () => {
-  return (
-    <div className='md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll' >
-        <h2 className='font-black text-3xl  text-center'>Listado Pacientes</h2>
-        <p className='text-xl mt-5 mb-10 text-center'>
-          Administra tus {''}
-          <span className='text-indigo-600 font-bold'>Pacientes y Citas</span>
-        </p>
-        <Paciente />
-        <Paciente />
-        <Paciente />
-        <Paciente />
-        <Paciente />
-        <Paciente />
-        <Paciente />
-    </div>
-  )
+interface Props {
+  pacientes: Array<Paciente>;
+  setPacientes?: React.Dispatch<React.SetStateAction<Paciente[]>>;
 }
 
-export default ListadoPacientes
+const ListadoPacientes = ({ pacientes }: Props): JSX.Element => {
+  return (
+    <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
+      <h2 className="font-black text-3xl  text-center">Listado Pacientes</h2>
+      <p className="text-xl mt-5 mb-10 text-center">
+        Administra tus {""}
+        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+      </p>
+
+      {pacientes.map((paciente) => (
+        <PacienteCita paciente={paciente} />
+      ))}
+    </div>
+  );
+};
+
+export default ListadoPacientes;
