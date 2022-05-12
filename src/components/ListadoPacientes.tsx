b5813915a08a79d462eb2ed547de6cd1b89fc8d9
Diego <diegoabustos24@gmail.com>
2022-05-12 18:47:06 -0500

    show conditionals text

--- src/components/ListadoPacientes.tsx
+++ src/components/ListadoPacientes.tsx
@@ -9,15 +9,31 @@ interface Props {
 const ListadoPacientes = ({ pacientes }: Props): JSX.Element => {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl  text-center">Listado Pacientes</h2>
-      <p className="text-xl mt-5 mb-10 text-center">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl  text-center">
+            Listado Pacientes
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
 
-      {pacientes.map(paciente => (
-        <PacienteCita key={paciente.id} paciente={paciente} />
-      ))}
+          {pacientes.map((paciente) => (
+            <PacienteCita key={paciente.id} paciente={paciente} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl  text-center">
+            NO hay Pacientes
+          </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            comienza agregando pacientes {""}
+            <span className="text-indigo-600 font-bold">y aparecerán en este lugar</span>
+          </p>
+        </>
+      )}
     </div>
   );
 };
